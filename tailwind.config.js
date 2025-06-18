7fe4b186ab647512c7b86e276d0a29917de1e41d
Jelena Tanasković <jelenatanaskovicj@gmail.com>
2025-06-18 12:59:10 +0200

    Fix invalid version and rebuild

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
+  darkMode: 'class', // ili 'media' ako želiš automatski prema sistemu
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#121212',
+        primary: '#ec4899', // Tailwind pink-500
+        text: '#e5e7eb', // Tailwind gray-100
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
-
 };
