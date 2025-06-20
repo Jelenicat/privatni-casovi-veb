ad13db9735732a9645c0544a8ad6b32b7f955892
Jelena Tanasković <jelenatanaskovicj@gmail.com>
2025-06-20 13:38:25 +0200

    kk

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,10 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav className="w-full bg-black bg-opacity-70 text-white px-6 py-4 flex justify-between items-center shadow-md fixed top-0 left-0 z-50">
-      <div className="text-2xl font-bold text-pink-400">
-        <Link to="/">Pronađi profesora</Link>
-      </div>
-
-      <div className="text-lg">
-        <Link to="/blog" className="hover:text-pink-400 transition">
-          Pitanja i odgovori
-        </Link>
-      </div>
+    <nav className="w-full bg-black bg-opacity-70 text-white px-6 py-4 flex justify-center items-center shadow-md fixed top-0 left-0 z-50">
+      <Link to="/blog" className="text-lg font-medium text-white hover:text-pink-400 transition">
+        Pitanja i odgovori
+      </Link>
     </nav>
   );
 }
